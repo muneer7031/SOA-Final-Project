938d793c0743792e76d47607d64443a98d02bdb4
Muneer7031 <muneer7031@gmail.com>
2025-04-12 22:58:13 -0400

    CI-3

--- user_service/app.py
+++ user_service/app.py
@@ -1,25 +1,35 @@
 from flask import Flask, request, jsonify
-from models import db, User
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-db.init_app(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'  # or any other DB
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(100))
+
+# Make sure this is called
 with app.app_context():
     db.create_all()
 
 @app.route('/register', methods=['POST'])
+@app.route('/register', methods=['POST'])
 def register():
-    data = request.json
-    new_user = User(name=data['name'], email=data['email'], password=data['password'])
-    db.session.add(new_user)
+    data = request.get_json()
+    existing_user = User.query.filter_by(email=data['email']).first()
+    if existing_user:
+        return jsonify({"message": "User already exists"}), 409  # 409 Conflict
+
+    user = User(name=data['name'], email=data['email'], password=data['password'])
+    db.session.add(user)
     db.session.commit()
     return jsonify({"message": "User registered successfully"}), 201
 
-@app.route('/users', methods=['GET'])
-def get_users():
-    users = User.query.all()
-    return jsonify([{"name": u.name, "email": u.email} for u in users]), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
