c3a6fbf4cf61830b11110fe19cee8f80f3aadc42
Muneer7031 <muneer7031@gmail.com>
2025-04-12 21:10:29 -0400

    user-service updated

--- user_service/app.py
+++ user_service/app.py
@@ -1,34 +1,22 @@
 from flask import Flask, request, jsonify
-import requests  # <-- New import
+from models import db, User
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db.init_app(app)
 
-users = []
+with app.app_context():
+    db.create_all()
 
 @app.route('/register', methods=['POST'])
 def register():
     data = request.json
-    users.append(data)
-
-    # Send notification to notification-service
-    try:
-        notify_response = requests.post(
-            "http://notification-service/send_notification",
-            json={
-                "name": data.get("name"),
-                "email": data.get("email")
-            },
-            timeout=5  # optional: avoid long waits
-        )
-        print("Notification response:", notify_response.text)
-    except Exception as e:
-        print("Failed to send notification:", str(e))
-
+    new_user = User(name=data['name'], email=data['email'], password=data['password'])
+    db.session.add(new_user)
+    db.session.commit()
     return jsonify({"message": "User registered successfully"}), 201
 
 @app.route('/users', methods=['GET'])
 def get_users():
-    return jsonify(users), 200
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    users = User.query.all()
+    return jsonify([{"name": u.name, "email": u.email} for u in users]), 200
